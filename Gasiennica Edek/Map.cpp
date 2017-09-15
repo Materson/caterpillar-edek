136bb1744f1c3eb1287c06142975deb3c532cacf
Mateusz Szymanowski <mathew.szyman@gmail.com>
2017-09-15 21:50:38 +0200

    CHANGE: data read

--- Gasiennica Edek/Map.cpp
+++ Gasiennica Edek/Map.cpp
@@ -14,13 +14,36 @@ Map::Map(int x, int y)
 {
 	area = new char[x*y];
 
-	char ch;
-	
+	char ch = ' ';
+	int k = 0;
 	for (int i = 0; i < x*y; i++)
 	{
-		scanf_s("%c", &ch);
-		//cin >> ch;.
-		area[i] = ch;
+		ch = ' ';
+		k = 0;
+		while (ch == ' ' || ch == '\n')
+		{
+			scanf_s("%c", &ch);
+			while (ch >= '0' && ch <= '9')
+			{
+				k += ch - 48;
+				k *= 10;
+				scanf_s("%c", &ch);
+			}
+		}
+		k /= 10;
+
+		if (k > 0)
+		{
+			for (int j = 0; j < k; j++, i++)
+			{
+				area[i] = ch;
+			}
+			i--;
+		}
+		else
+		{
+			area[i] = ch;
+		}
 
 	}
 }
@@ -34,7 +57,7 @@ void Map::show()
 	for (int i = 0; i < x*y; i++)
 	{
 		
-		cout << area[i];
+		printf_s("%c ", area[i]);
 		
 		if ((i+1) % x == 0)
 		{
