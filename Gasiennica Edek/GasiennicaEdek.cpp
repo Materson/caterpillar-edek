283f4cdeff6c672ffe422ec7770579bfa0155557
Mateusz Szymanowski <mathew.szyman@gmail.com>
2017-09-17 13:34:07 +0200

    REMOVE: unnecessary things for 16th test

--- Gasiennica Edek/GasiennicaEdek.cpp
+++ Gasiennica Edek/GasiennicaEdek.cpp
@@ -8,22 +8,12 @@ int main()
 {
 	int x = 0, y = 0, z = 0;
 	scanf_s("%d%d%d", &x, &y, &z);
-	if (x * y != 40000)
-	{
-		Map map(x, y, z);
-		//map.show();
-		map.round();
-		map.printEdekPosition();
-		map.show();
-
-	}
-	else
-	{
-		printf_s("Zegnaj, okrutny swiecie!\n6 8");
-	}
+	Map map(x, y, z);
+	map.round();
+	map.printEdekPosition();
+	map.show();
 
 
-	//delete(map);
     return 0;
 }
 
