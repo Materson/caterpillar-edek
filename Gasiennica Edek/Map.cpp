ce9f57f67745e6bbffaa01789a8ca7b8202ddcee
Mateusz Szymanowski <mathew.szyman@gmail.com>
2017-09-20 14:27:33 +0200

    COMMENT: unnecessary addField and findField

--- Gasiennica Edek/Map.cpp
+++ Gasiennica Edek/Map.cpp
@@ -165,14 +165,8 @@ char Map::checkPlace(int x, int y)
 	{
 		return 'T';
 	}
-	field *find = new field;
-	find->x = x;
-	find->y = y;
-	find->content = '.';
-	findField(find);
-	char c = find->content;
-	delete(find);
-	return c;
+	
+	return area[x][y];
 }
 
 void Map::printEdekPosition()
@@ -182,146 +176,141 @@ void Map::printEdekPosition()
 
 void Map::paint(int x, int y, char c)
 {
-	field *find = new field;
-	find->x = x;
-	find->y = y;
-	find->content = c;
-	findField(find);
-	delete(find);
+	area[x][y] = c;
 }
 
-void Map::addField(int x, int y, char con)
-{
-	if (x == 0 && y == 0)
-	{
-		anchor->content = con;
-	}
-	else
-	{
-		field *newf = new field;
-		newf->content = con;
-		newf->x = x;
-		newf->y = y;
-		newf->right = NULL;
-		newf->down = NULL;
+//void Map::addField(int x, int y, char con)
+//{
+//	if (x == 0 && y == 0)
+//	{
+//		anchor->content = con;
+//	}
+//	else
+//	{
+//		field *newf = new field;
+//		newf->content = con;
+//		newf->x = x;
+//		newf->y = y;
+//		newf->right = NULL;
+//		newf->down = NULL;
+//
+//		field *tmp = anchor;
+//		field *levelup=NULL;
+//		while ( tmp->down != NULL && tmp->down->y <= y)
+//		{
+//			levelup = tmp;
+//			tmp = tmp->down;
+//		}
+//
+//		if (tmp->y == y)
+//		{
+//			while (tmp->right != NULL && tmp->right->x < x)
+//			{
+//				tmp = tmp->right;
+//			}
+//
+//			if (tmp->x > x)
+//			{
+//				newf->right = tmp;
+//				if (levelup->down->down != NULL)
+//					newf->down = levelup->down->down;
+//				levelup->down = newf;
+//			}
+//			else
+//			{
+//				if (tmp->right != NULL)
+//				{
+//					newf->right = tmp->right;
+//				}
+//				tmp->right = newf;
+//			}
+//		}
+//		else
+//		{
+//			if (tmp->down != NULL)
+//			{
+//				newf->down = tmp->down;
+//			}
+//			tmp->down = newf;
+//		}
+//	}
+//}
 
-		field *tmp = anchor;
-		field *levelup=NULL;
-		while ( tmp->down != NULL && tmp->down->y <= y)
-		{
-			levelup = tmp;
-			tmp = tmp->down;
-		}
-
-		if (tmp->y == y)
-		{
-			while (tmp->right != NULL && tmp->right->x < x)
-			{
-				tmp = tmp->right;
-			}
-
-			if (tmp->x > x)
-			{
-				newf->right = tmp;
-				if (levelup->down->down != NULL)
-					newf->down = levelup->down->down;
-				levelup->down = newf;
-			}
-			else
-			{
-				if (tmp->right != NULL)
-				{
-					newf->right = tmp->right;
-				}
-				tmp->right = newf;
-			}
-		}
-		else
-		{
-			if (tmp->down != NULL)
-			{
-				newf->down = tmp->down;
-			}
-			tmp->down = newf;
-		}
-	}
-}
-
-void Map::findField(field * find)
-{
-	field *tmp = anchor;
-	field *levelup = NULL;
-	while (tmp->down != NULL && tmp->down->y <= find->y)
-	{
-		levelup = tmp;
-		tmp = tmp->down;
-	}
-
-	if (tmp->y == find->y)
-	{
-		while (tmp->right != NULL && tmp->right->x <= find->x)
-		{
-			tmp = tmp->right;
-		}
-
-		if (tmp->x == find->x)
-		{
-			if (tmp->content == '.' && find->content != '.')
-			{
-				tmp->content = find->content;	//paint
-			}
-			else
-			{
-				find->content = tmp->content;	//checkPlace
-				if (tmp->content == 'G')			//delete mushroom
-					tmp->content = '.';
-			}
-		}
-		else
-		{
-			if (find->content != '.')	//add pool
-			{
-				field *pool = new field;
-				pool->x = find->x;
-				pool->y = find->y;
-				pool->content = find->content;
-				pool->right = NULL;
-				pool->down = NULL;
-
-				if (tmp->x > find->x)
-				{
-					pool->right = tmp;
-					if(levelup->down->down != NULL)
-						pool->down = levelup->down->down;
-					levelup->down = pool;
-				}
-				else
-				{
-					if (tmp->right != NULL)
-					{
-						pool->right = tmp->right;
-					}
-					tmp->right = pool;
-				}
-			}
-		}
-	}
-	else
-	{
-		if (find->content != '.')
-		{
-			field *pool = new field;
-			pool->x = find->x;
-			pool->y = find->y;
-			pool->content = find->content;
-			pool->right = NULL;
-			pool->down = NULL;
-
-			if (tmp->down != NULL)
-			{
-				pool->down = tmp->down;
-			}
-			tmp->down = pool;
-		}
-	}
-}
+//void Map::findField(field * find)
+//{
+//	field *tmp = anchor;
+//	field *levelup = NULL;
+//	while (tmp->down != NULL && tmp->down->y <= find->y)
+//	{
+//		levelup = tmp;
+//		tmp = tmp->down;
+//	}
+//
+//	if (tmp->y == find->y)
+//	{
+//		while (tmp->right != NULL && tmp->right->x <= find->x)
+//		{
+//			tmp = tmp->right;
+//		}
+//
+//		if (tmp->x == find->x)
+//		{
+//			if (tmp->content == '.' && find->content != '.')
+//			{
+//				tmp->content = find->content;	//paint
+//			}
+//			else
+//			{
+//				find->content = tmp->content;	//checkPlace
+//				if (tmp->content == 'G')			//delete mushroom
+//					tmp->content = '.';
+//			}
+//		}
+//		else
+//		{
+//			if (find->content != '.')	//add pool
+//			{
+//				field *pool = new field;
+//				pool->x = find->x;
+//				pool->y = find->y;
+//				pool->content = find->content;
+//				pool->right = NULL;
+//				pool->down = NULL;
+//
+//				if (tmp->x > find->x)
+//				{
+//					pool->right = tmp;
+//					if(levelup->down->down != NULL)
+//						pool->down = levelup->down->down;
+//					levelup->down = pool;
+//				}
+//				else
+//				{
+//					if (tmp->right != NULL)
+//					{
+//						pool->right = tmp->right;
+//					}
+//					tmp->right = pool;
+//				}
+//			}
+//		}
+//	}
+//	else
+//	{
+//		if (find->content != '.')
+//		{
+//			field *pool = new field;
+//			pool->x = find->x;
+//			pool->y = find->y;
+//			pool->content = find->content;
+//			pool->right = NULL;
+//			pool->down = NULL;
+//
+//			if (tmp->down != NULL)
+//			{
+//				pool->down = tmp->down;
+//			}
+//			tmp->down = pool;
+//		}
+//	}
+//}
